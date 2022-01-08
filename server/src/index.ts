6f82c03c1a634f5c3dd33ce47217117f69794cf8
martalewan <63184918+martalewan@users.noreply.github.com>
2022-01-08 22:51:26 +0100

    modify node modules

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,8 +8,9 @@ import handleNewClient from './ws'
 const app = express();
 expressWs(app);
 dotenv.config();
+console.log(process.env);
 
-const port = 80;
+const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
