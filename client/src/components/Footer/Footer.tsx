89fd0245e86886a8941188cd98e8603361b332f4
martalewan <63184918+martalewan@users.noreply.github.com>
2022-01-09 18:35:48 +0100

    Update mains page text and elements of footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => (
     <footer className="footer-wrapper">
         <p className="footer-wrapper__link">Made by <a href="https://martalewan.com/">Marta Lewan</a></p>
-        <p className="footer-wrapper__link">2021</p>
+        <p className="footer-wrapper__link">2022</p>
     </footer>
 );
 
